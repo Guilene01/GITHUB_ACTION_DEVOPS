7a7ed666f991240cc3ed1df89cdd279b4a3937dc
guilene1 <guilenetiako@utrains.org>
2026-06-15 13:52:30 -0400

    new code

--- src/main/java/com/devops/utrains/App.java
+++ src/main/java/com/devops/utrains/App.java
@@ -3,7 +3,7 @@ package com.devops.utrains;
 public class App {
 
     public String greet(String name) {
-        return "Hello , " + name + "!";
+        return "Hello l.gb, " + name + "!";
     }
 
     public static void main(String[] args) {
